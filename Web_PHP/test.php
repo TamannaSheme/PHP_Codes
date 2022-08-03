c0b6117a5dbe1b0b17505257400ecebc5b00090c
Syeda Tamanna Sheme <71142803+TamannaSheme@users.noreply.github.com>
2022-08-03 22:13:21 +0600

    Add files via upload
    Added

---
+++ Web_PHP/test.php
@@ -0,0 +1,79 @@
+<?php 
+	
+	/*
+	echo '<h1>Hello world</h1>';
+	print 'Hello world from the print function <br>';
+	
+	
+	$x = 10;
+	echo $x;
+	$x = 'This is text';
+	echo $x;
+	
+	*/
+	/*
+	$x = 10;
+	$y = 20;
+	$sum = $y % $x;
+	echo 'Ths sum is=' . $sum;
+	*/
+	
+	/*
+	$num = 75;
+	
+	if($num == 80) {
+		echo 'A+';
+	} else if($num >= 70 && $num <80) {
+		echo 'A';
+	} else {
+		echo 'F';
+	}
+	*/
+	/*
+	for($i=0; $i<10; $i++) {
+		echo $i . "<br>";
+	}*/
+	/*
+	$i=0;
+	while($i<10) {
+		echo $i;
+		$i++;
+		if($i==8) {
+			break;
+		} 
+	}
+	*/
+	/*
+	$num_array = array(10, 20, 30, 40);
+	echo $num_array[0];
+	*/
+	/*
+	$num_array = array(
+		array(10,20,30),
+		array(100,200,300),
+		array(1000,2000,3000)
+	);
+	
+	echo $num_array[1][0];
+	*/
+	/*
+	function add($num1, $num2) {
+		//echo $num1 + $num2;
+		return $num1 + $num2;
+	}
+	//add(10,20);
+	$sum = add(1000,2000);
+	echo $sum;
+	
+	*/
+	// 
+	// $persons = array('james' => 100, 'bond' => 200, 'tom' => 5000);
+	
+	
+	// echo $persons['james'] + $persons['bond'];
+	
+	
+	
+	
+	
+?>
